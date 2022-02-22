2e566b63d452d3d6dd6bd7f7a9b71d3a27966ea0
Braydon Edwards <90294227+steadyeddie03@users.noreply.github.com>
2022-02-22 21:03:49 +0000

    git commit -m "render pyramid to the DOM instead of the console"

--- mario.js
+++ mario.js
@@ -1,4 +1,4 @@
-
+  
 printPyramid(prompt("Please enter pyramid height: "));
 // TODO #2
 // Take in user input for the height
@@ -16,29 +16,46 @@ printPyramid(prompt("Please enter pyramid height: "));
  *      ######
  */
 function printPyramid(height) {
+  //create a variable to change the pyramidDiv
+  let pyramidDiv = document.getElementById("pyramid");
+  //get the construction div
+  let constructDiv =  document.getElementById("construction");
+  //clear it out
+  constructDiv.innerHTML = "";
   for (var row = 0; row < height; row++) {
   
-          // figure out how many bricks in this row (aka the '#' characters)
-          var numBricks = row + 2;
-  
-          // figure out how many space characters
-          var numSpaces = height - row - 1;
-  
-          // build up a string for this row. Starts empty...
-          var rowStr = "";
-  
-          // add the spaces
-          for (var i = 0; i < numSpaces; i++) {
-              rowStr += " ";
-          }
-  
-          // add the bricks
-          for (var i = 0; i < numBricks; i++) {
-              rowStr += "#";
-          }
-  
-          // print the string
-          console.log(rowStr);
+    // figure out how many bricks in this row (aka the '#' characters)
+    var numBricks = row + 2;
+
+    // figure out how many space characters
+    var numSpaces = height - row - 1;
+
+    // build up a string for this row. Starts empty...
+    var rowStr = "";
+
+    // add the spaces
+    for (var i = 0; i < numSpaces; i++) {
+        rowStr += "&nbsp;";
+    }
+
+    // add the bricks
+    for (var i = 0; i < numBricks; i++) {
+        rowStr += "#";
+    }
+
+    // print the string
+    console.log(rowStr);
+    pyramidDiv.innerHTML += "<p>" + rowStr + "</p>" ;
+    //  <p> rowStr </p>
+    
+    //build a paragraph element using rowstr
+    //add the paragraph to the pyramid div
+          
+
+          
     }
 }
 
+//pyramidDiv = document.getElementById("pyramid");
+
+//pyramidDiv.innerHTML = "hello";
